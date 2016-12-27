1fd8601d2c268bf403c8a90ae45dbc40662820b9
Ren Bettendorf <renaldorini@gmail.com>
2016-12-26 19:43:16 -0800

    Minor change from IParkingGarage to ParkingGarage

--- src/server/ParkingGarageServer.java
+++ src/server/ParkingGarageServer.java
@@ -8,7 +8,7 @@ public class ParkingGarageServer {
 	
 	public ParkingGarageServer(String url, int occupationSize) throws RemoteException {
 		try {
-			IParkingGarage garage = new ParkingGarage(occupationSize);
+			ParkingGarage garage = new ParkingGarage(occupationSize);
 			Naming.rebind(url, garage);
 			System.out.println("Parking Garage server running...");
 		} catch (RemoteException re) {
